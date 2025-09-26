3c7abcec2fe5baa27a5743675375ec557110787c
dcz <gilapfco.dcz@porcupinefactory.org>
2025-09-26 10:56:26 +0200

    local changes

--- update.sh
+++ update.sh
@@ -10,9 +10,9 @@ set -e
 
 rm -rf src
 mkdir src
-svd patch patches/gd32vf103.yaml
-svd2rust --target riscv -i GD32VF103.svd.patched
-form -i lib.rs -o src
+#svd patch patches/gd32vf103.yaml
+../svd2rust/target/debug/svd2rust --target riscv -i GD32VF103.svd.patched
+../form/target/debug/form -i lib.rs -o src
 rm lib.rs
 cargo fmt
 grep -E 'feature = "rt"|extern crate riscv_rt' src/lib.rs | tee librs-patch
